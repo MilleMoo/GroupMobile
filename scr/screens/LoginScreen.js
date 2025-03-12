8d26cbc0d5260f86544e11d9d42759834f30ce13
MilleMoo <118721832+MilleMoo@users.noreply.github.com>
2025-03-12 19:59:23 +0700

    Update LoginScreen.js

--- scr/screens/LoginScreen.js
+++ scr/screens/LoginScreen.js
@@ -1,6 +1,8 @@
 import React ,{ useState } from "react";
 import { View, Text, StyleSheet, TextInput, Button, Alert,TouchableOpacity } from "react-native"
 import axios from "axios";
+import Icon from "react-native-vector-icons/Feather";
+import Icon1 from "react-native-vector-icons/Fontisto";
 
 
 const LoginScreen = ({ navigation }) => {
@@ -19,21 +21,32 @@ const LoginScreen = ({ navigation }) => {
     }
     return (
         <View style={style.container}>
-            <TextInput
-                style={style.input}
-                placeholder="Username"
-                onChangeText={setUsername}
-                value={Username}
+            <View style = {{ justifyContent: "center", alignItems: "center" }}>
+                <Text style= {{ fontSize: 30, color: "white", fontWeight: "bold" }}>
+                    Login
+                </Text>
+            </View>
+            <View style = {style.inputContainer}>
+                <TextInput
+                    style = {style.button} // เปลี่ยนสีเส้นขอบเมื่อมี error
+                    placeholder = "Username" // ถ้ามี error ให้ขึ้นข้อความแทน
+                    value = {Username}
+                    onChangeText = {setUsername}
                 />
-            <TextInput
-                style={style.input}
-                placeholder="Password"
-                onChangeText={setPassword}
-                value={Password}
-                secureTextEntry
-            />
+        <Icon name = "user" size = {20} color = "white" style = {style.iconemail}/>    
+        </View>
+        <View style = {style.inputContainer}>
+                <TextInput
+                    style = {style.button} // เปลี่ยนสีเส้นขอบเมื่อมี error
+                    placeholder = "Username" // ถ้ามี error ให้ขึ้นข้อความแทน
+                    value = {Password}
+                    onChangeText = {setPassword}
+                />
+            <Icon name = "lock" size = {20} color = "white" style = {style.iconemail}/>    
+        </View>
+        
             <View style={{ flexDirection: "row" }}>
-                <TouchableOpacity >
+                <TouchableOpacity onPress={handleLogin}>
                     <View style={style.Login}>
                         <Text style={{ color: "white", fontWeight: "bold", fontSize: 20, }}>
                             Sign In
@@ -42,17 +55,17 @@ const LoginScreen = ({ navigation }) => {
                 </TouchableOpacity>
                 <TouchableOpacity >
                     <View style={style.FPassword}>
-                        <Text style={{ color: "black",fontSize: 20, }}>
+                        <Text style={{ color: "white",fontSize: 20, }}>
                             Forgot Password
                         </Text>
                     </View>
                 </TouchableOpacity>
             </View>
             <View style={{ alignItems: "flex-end",marginTop: 50,}}>
-                <TouchableOpacity >
+                <TouchableOpacity onPress={() => navigation.navigate("Register")}>
                         <View style={style.FPassword}>
-                            <Text style={{ color: "black",fontSize: 20, }}>
-                                Register New
+                            <Text style={{ color: "white",fontSize: 20, }}>
+                                Sign Up
                             </Text>
                         </View>
                     </TouchableOpacity>
@@ -66,7 +79,7 @@ const style = StyleSheet.create({
         flex: 5,
         justifyContent: "center",
         padding: 16,
-        backgroundColor: "#1a649a "
+        backgroundColor: "#1a649a"
     },
     input: {
         height: 45,
@@ -99,6 +112,47 @@ const style = StyleSheet.create({
         fontSize: 10,
         marginTop: 30,
         marginLeft: 50,
+    },
+    button: {
+        width: "100%",
+        borderRadius: 5,
+        marginVertical: "15",
+        backgroundColor: "rgba(240, 233, 233, 0.25)", // ทำให้โปร่งใส่
+    },
+    buttonText: {
+        color: "white",
+        fontSize: 16,
+        fontWeight: 'bold',
+        textAlign:'center',
+    },
+    buttonRegis: {
+        paddingVertical: 15, // เพิ่ม ระยะห่างด้านบน-ล่างให้มันเท่ากัน paddingTop กับ paddingButton
+        paddingHorizontal: 20, // เพิ่ม ระยะห่างซ้าย-ขวาให้มันเท่ากัน paddingRight กับ paddingLeft
+        borderRadius: 25, //ทำให้เป็นกลม ๆ 
+        marginVertical: 5, // เพิ่ม ระยะห่างด้านบน-ล่างให้มันเท่ากัน marginTop กับ marginButton 1
+        width: "40%",
+        alignSelf: "center",
+        backgroundColor: "rgba(43, 175, 43, 0.5)", // ทำให้โปร่งใส่
+    },
+    error:{
+        color: "red",
+        marginVertical: 5,
+    },
+    inputContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+    },
+    icon: {
+        position: "absolute",
+        left: 340,
+    },
+    iconemail: {
+        position: "absolute",
+        left: 370,
+    },
+    inputError: {
+        borderColor: "red", // เปลี่ยนเส้นขอบเป็นสีแดง
+        borderWidth: 1,
     },
 })
 
