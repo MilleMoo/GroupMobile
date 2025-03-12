572eb889755f78a71c279f2292294489fa2dc9f9
MilleMoo <118721832+MilleMoo@users.noreply.github.com>
2025-03-12 19:57:54 +0700

    Update Api.js

--- Api.js
+++ Api.js
@@ -1,16 +1,28 @@
 import axios from "axios";
 
-const API_URL = "http://192.168.11.125:5000"; // แก้ URL ให้ถูกต้อง
+const API_URL = "http://10.36.17.45:5000";
+
+export const LoginUser = async (username, password) => {
+    try {
+        const response = await axios.post(`${API_URL}/Login`, {
+            username,
+            password,
+        });
+        return response.data.token;
+    } catch (error) {
+        throw new Error(error.response?.data?.message || "Invalid Login");
+    }
+};
 
 export const registerUser = async (username, password) => {
-  // เพิ่ม async
-  try {
-    const response = await axios.post(`${API_URL}/register`, {
-      username,
-      password,
-    });
-    return response.data;
-  } catch (error) {
-    throw new Error(error.response?.data?.message || "Error registering user");
-  }
-};
+    // เพิ่ม async
+    try {
+      const response = await axios.post(`${API_URL}/register`, {
+        username,
+        password,
+      });
+      return response.data;
+    } catch (error) {
+      throw new Error(error.response?.data?.message || "Error registering user");
+    }
+  };
