ede3e2ec3374b930168d54318f0d3749b98be4b4
MilleMoo <118721832+MilleMoo@users.noreply.github.com>
2025-03-12 18:47:32 +0700

    Add files via upload

--- scr/screens/LoginScreen.js
+++ scr/screens/LoginScreen.js
@@ -1,62 +1,105 @@
-import React, { useState } from "react";
-import { View, TextInput, StyleSheet } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
+import React ,{ useState } from "react";
+import { View, Text, StyleSheet, TextInput, Button, Alert,TouchableOpacity } from "react-native"
+import axios from "axios";
 
-const LoginScreen = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
 
+const LoginScreen = ({ navigation }) => {
+    const [Username, setUsername] = useState("");
+    const [Password, setPassword] = useState("");
+
+    const handleLogin = async () => {
+        try {
+            const response = await LoginUser(Username, Password);
+            Alert.alert("Login Successful", `Token: ${response}`);
+            navigation.navigate("Home");
+
+        } catch (error) {
+            Alert.alert("login Failed!", error.message)
+        }
+    }
     return (
-        <View style={styles.container}>
-            {/* ช่องกรอก Email */}
-            <View style={styles.inputContainer}>
-                
-                <TextInput
-                    style={styles.input}
-                    placeholder="Enter Your Email"
-                    placeholderTextColor="#aaa"
-                    value={email}
-                    onChangeText={setEmail}
+        <View style={style.container}>
+            <TextInput
+                style={style.input}
+                placeholder="Username"
+                onChangeText={setUsername}
+                value={Username}
                 />
+            <TextInput
+                style={style.input}
+                placeholder="Password"
+                onChangeText={setPassword}
+                value={Password}
+                secureTextEntry
+            />
+            <View style={{ flexDirection: "row" }}>
+                <TouchableOpacity >
+                    <View style={style.Login}>
+                        <Text style={{ color: "white", fontWeight: "bold", fontSize: 20, }}>
+                            Sign In
+                        </Text>
+                    </View>
+                </TouchableOpacity>
+                <TouchableOpacity >
+                    <View style={style.FPassword}>
+                        <Text style={{ color: "black",fontSize: 20, }}>
+                            Forgot Password
+                        </Text>
+                    </View>
+                </TouchableOpacity>
             </View>
-
-            {/* ช่องกรอก Password */}
-            <View style={styles.inputContainer}>
-                
-                <TextInput
-                    style={styles.input}
-                    placeholder="Password"
-                    placeholderTextColor="#aaa"
-                    secureTextEntry
-                    value={password}
-                    onChangeText={setPassword}
-                />
+            <View style={{ alignItems: "flex-end",marginTop: 50,}}>
+                <TouchableOpacity >
+                        <View style={style.FPassword}>
+                            <Text style={{ color: "black",fontSize: 20, }}>
+                                Register New
+                            </Text>
+                        </View>
+                    </TouchableOpacity>
             </View>
         </View>
-    );
-};
+    )
+}
 
-const styles = StyleSheet.create({
+const style = StyleSheet.create({
     container: {
-        padding: 20,
-    },
-    inputContainer: {
-        flexDirection: "row",
-        alignItems: "center",
-        backgroundColor: "#f5f5f5",
-        borderRadius: 10,
-        paddingHorizontal: 15,
-        marginBottom: 15,
-    },
-    icon: {
-        marginRight: 10,
+        flex: 5,
+        justifyContent: "center",
+        padding: 16,
+        backgroundColor: "#1a649a "
     },
     input: {
-        flex: 1,
-        height: 50,
-        fontSize: 16,
-        color: "#333",
+        height: 45,
+        borderColor: "#ccc",
+        borderWidth: 1,
+        marginBottom: 12,
+        paddingHorizontal: 8,
     },
-});
+    Login:{
+        borderRadius: 50,
+        borderWidth: 1,
+        height: 50,
+        width: 130,
+        justifyContent: "center",
+        alignItems: "center",
+        textAlign: "auto",
+        fontSize: 10,
+        marginTop: 30,
+        marginLeft: 40,
+        backgroundColor: "#275b81 ",
+        borderColor: "white",
+    },
+    FPassword:{
+        borderRadius: 50,
+        height: 50,
+        width: 170,
+        justifyContent: "center",
+        alignItems: "center",
+        textAlign: "auto",
+        fontSize: 10,
+        marginTop: 30,
+        marginLeft: 50,
+    },
+})
 
-export default LoginScreen;
+export default LoginScreen
